ebc4e8a4f9e79a547aa830ffbe11f92073fe23b1
Senay1675 <senay_93@hotmail.com>
2024-04-18 00:55:28 +0200

    klar med funktionaliteten, har bara styling kvar

--- script.js
+++ script.js
@@ -14,12 +14,6 @@ async function fetchPokemonList() {
 
 fetchPokemonList()
 
-
-
-
-
-
-
 // Den här funktionen fyller dropdown listan med alla pokemon alternativ
 
 async function populatePokemonDropdown() {
@@ -34,22 +28,27 @@ async function populatePokemonDropdown() {
 }
 populatePokemonDropdown();
 
-
-
 // Denna funktion tar en Pokémon in som data och visar dess detaljer på sidan genom att 
 // ändra innehållet i olika element baserat på Pokémon-informationen.
 
 function displayPokemonDetails(pokemon) {
     console.log(pokemon);
     document.getElementById('pokemon-name').textContent = pokemon.name;
-    document.getElementById('pokemon-image').src = pokemon.sprites.front_default;
+    // document.getElementById('pokemon-image').src = pokemon.sprites.front_default;
+    const imageUrl = pokemon.sprites.other['dream_world']['front_default'] || pokemon.sprites.front_default;
+    document.getElementById('pokemon-image').src = imageUrl;
     document.getElementById('pokemon-types').textContent = `Types: ${pokemon.types.map(type => type.type.name).join(', ')}`;
     document.getElementById('pokemon-weight').textContent = `Weight: ${pokemon.weight}`;
     document.getElementById('pokemon-height').textContent = `Height: ${pokemon.height}`;
     
-    // const stats = pokemon.stats.map(stat => `${stat.stat.name}: ${stat.base_stat}`).join(', ');
-    const stats = pokemon.stats.map(stat => `${stat.stat.name}: ${parseInt(stat.base_stat)}`).join(', ');
-    document.getElementById('pokemon-stats').textContent = `Stats: ${stats}`;
+    
+    // const stats = pokemon.stats.map(stat => `${stat.stat.name}: ${parseInt(stat.base_stat)}`).join(', ');
+    // document.getElementById('pokemon-stats').textContent = `Stats: ${stats}`;
+
+    const statsList = document.getElementById('pokemon-stats');
+    statsList.innerHTML = ''; // Rensa innehållet för att undvika dubbletter
+    const stats = pokemon.stats.map(stat => `<li>${stat.stat.name}: ${parseInt(stat.base_stat)}</li>`).join('');
+    statsList.innerHTML = `<ul>${stats}</ul>`;
 
     document.getElementById('pokemon-details').style.display = 'block';
     console.log(stats);
@@ -112,14 +111,20 @@ populatePokemonDropdown2();
 function displayPokemonDetails2(pokemon) {
     console.log(pokemon);
     document.getElementById('pokemon-name-2').textContent = pokemon.name;
-    document.getElementById('pokemon-image-2').src = pokemon.sprites.front_default;
+    // document.getElementById('pokemon-image-2').src = pokemon.sprites.front_default;
+    const imageUrl2 = pokemon.sprites.other['dream_world']['front_default'] || pokemon.sprites.front_default;
+    document.getElementById('pokemon-image-2').src = imageUrl2;
     document.getElementById('pokemon-types-2').textContent = `Types: ${pokemon.types.map(type => type.type.name).join(', ')}`;
     document.getElementById('pokemon-weight-2').textContent = `Weight: ${pokemon.weight}`;
     document.getElementById('pokemon-height-2').textContent = `Height: ${pokemon.height}`;
     
    
-    const stats = pokemon.stats.map(stat => `${stat.stat.name}: ${parseInt(stat.base_stat)}`).join(', ');
-    document.getElementById('pokemon-stats-2').textContent = `Stats: ${stats}`;
+    // const stats = pokemon.stats.map(stat => `${stat.stat.name}: ${parseInt(stat.base_stat)}`).join(', ');
+    // document.getElementById('pokemon-stats-2').textContent = `Stats: ${stats}`;
+    const statsList = document.getElementById('pokemon-stats-2');
+    statsList.innerHTML = ''; // Rensa innehållet för att undvika dubbletter
+    const stats = pokemon.stats.map(stat => `<li>${stat.stat.name}: ${parseInt(stat.base_stat)}</li>`).join('');
+    statsList.innerHTML = `<ul>${stats}</ul>`;
 
     document.getElementById('pokemon-details-2').style.display = 'block';
     console.log(stats);
@@ -180,6 +185,8 @@ class Pokemon{
 
         console.log(pokemon1.stats);
 
+        // denna object keys funktionen plussar ihop alla stats för att se vilken som är högst totalt
+
         Object.keys(pokemon1.stats).forEach((stat)=>{
             console.log(pokemon1.stats[stat]);
             allPokeStats1 += pokemon1.stats[stat]
@@ -196,46 +203,86 @@ class Pokemon{
         //jämför height
         if (pokemon1.height > pokemon2.height) {
             firstPokemon++; 
-            battleText.innerHTML = `${pokemon1.name} is taller than ${pokemon2.name} `;
-            //pokemon document.querySelector()
-            
+            battleText.innerHTML = `${pokemon1.name} is taller than ${pokemon2.name}<br>`;
+            document.getElementById('pokemon-height').classList.add("heavier");
+            document.getElementById(`pokemon-height-2`).classList.remove("heavier");
+            document.getElementById(`pokemon-height`).classList.remove("lighter");
+            document.getElementById('pokemon-height-2').classList.add("lighter");   
         }
         else if(pokemon2.height > pokemon1.height){
             secondPokemon++;
-            battleText.innerHTML = `${pokemon2.name} is taller than ${pokemon1.name} `;
+            battleText.innerHTML = `${pokemon2.name} is taller than ${pokemon1.name}!<br>`;
+            document.getElementById('pokemon-height-2').classList.add("heavier");
+            document.getElementById(`pokemon-height`).classList.remove("heavier");
+            document.getElementById(`pokemon-height-2`).classList.remove("lighter");
+            document.getElementById('pokemon-height').classList.add("lighter");   
+
         }
         else if (pokemon1.height === pokemon2.height) {
-            battleText.innerHTML = `${pokemon2.name} Is equally tall as ${pokemon1.name} `
+            battleText.innerHTML = `${pokemon2.name} Is equally tall as ${pokemon1.name}!<br>`
+
         }
 
         // jämför weight
 
         if (pokemon1.weight > pokemon2.weight) {
             firstPokemon++; 
-            battleText.innerHTML = `${pokemon1.name} is heavier than ${pokemon2.name} `;
-            
-        }
+            battleText.innerHTML = `${pokemon1.name} is heavier than ${pokemon2.name}!<br> `;
+            document.getElementById('pokemon-weight').classList.add("heavier");
+            document.getElementById(`pokemon-weight-2`).classList.remove("heavier");
+            document.getElementById(`pokemon-weight`).classList.remove("lighter");
+            document.getElementById('pokemon-weight-2').classList.add("lighter");
+           
+            }
         else if(pokemon2.weight > pokemon1.weight){
             secondPokemon++;
-            battleText.innerHTML = `${pokemon2.name} is heavier than ${pokemon1.name} `;
+            battleText.innerHTML = `${pokemon2.name} is heavier than ${pokemon1.name}!<br>`;
+            document.getElementById('pokemon-weight-2').classList.add("heavier");
+            document.getElementById(`pokemon-weight`).classList.remove("heavier");
+            document.getElementById(`pokemon-weight-2`).classList.remove("lighter");
+            document.getElementById('pokemon-weight').classList.add("lighter");
+
         }
         else if (pokemon1.weight === pokemon2.weight){
-            battleText.innerHTML = `${pokemon1.name} is equally heavy as ${pokemon2.name} `;
+            battleText.innerHTML = `${pokemon1.name} is equally heavy as ${pokemon2.name}! <br> `;
         }
 
         //jämför stats
 
-   
+        if (allPokeStats1 > allPokeStats2) {
+            firstPokemon++;
+            battleText.innerHTML = `${pokemon1.name} has higher stats than ${pokemon2.name}!<br>`;
+            document.querySelector(`#pokemon-stats`).classList.add("green");
+            document.querySelector(`#pokemon-stats`).classList.remove("red");
+            document.querySelector(`#pokemon-stats-2`).classList.add("red");
+            document.querySelector(`#pokemon-stats-2`).classList.remove("green");
+
+
+        } else if(allPokeStats2 > allPokeStats1) {
+            secondPokemon++;
+            battleText.innerHTML = `${pokemon2} has higher stats than ${pokemon1.name}!<br>`;
+            document.querySelector(`#pokemon-stats-2`).classList.add("green");
+            document.querySelector(`#pokemon-stats`).classList.remove("green");
+            document.querySelector(`#pokemon-stats`).classList.add("red");
+            document.querySelector(`#pokemon-stats-2`).classList.remove("red");
+
+        }
+        else if(allPokeStats1 === allPokeStats2){
+            battleText.innerHTML = `${pokemon1.name} has the same stats as ${pokemon2.name}!<br>`
+            
+        }
+
+        // Jämför alla kategorier
 
         if (firstPokemon > secondPokemon) { 
-            battleText.innerHTML += `${pokemon1.name} is stronger than ${pokemon2.name} `;
+            battleText.innerHTML += `${pokemon1.name} wins in more categories than ${pokemon2.name}!<br>`;
 
         }
         else if (firstPokemon < secondPokemon){
-            battleText.innerHTML += `${pokemon2.name} is stronger than ${pokemon1.name}`;
+            battleText.innerHTML += `${pokemon2.name} wins in more categories than  ${pokemon1.name}!<br>`;
         }
         else if (firstPokemon === secondPokemon){
-            battleText.innerHTML += `${pokemon2.name} is equally strong as ${pokemon1.name}`;
+            battleText.innerHTML += `${pokemon2.name} is equally strong as ${pokemon1.name}!<br> `;
         }
        //
        
